bd00c85683732d8ca60b4a9218764ed48b09f70c
Faiz Alam <faiz276482@gmail.com>
2021-06-01 13:45:58 +0530

    Dashboard ui setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/layout/NavBar'
+import Dashboard from './components/dashboard/Dashboard'
 
 function App() {
   return (
     <Router>
       <div className="App">
         <NavBar />
+        <Switch>
+          <Route path="/" component={Dashboard}/>
+        </Switch>
       </div>
     </Router>
     
