be99b45bd8bf72062e89264454954b1645f8fbfe
Faiz Alam <faiz276482@gmail.com>
2021-06-01 13:15:58 +0530

    NavBar ui setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import NavBar from './components/layout/NavBar'
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <h1>MarioPlan</h1>
+        <NavBar />
       </div>
     </Router>
     
