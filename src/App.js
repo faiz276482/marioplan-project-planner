6d4369123c2d8e9ba7cb59a890934779159ac9b4
Faiz Alam <faiz276482@gmail.com>
2021-06-01 16:51:23 +0530

    Create Project ui added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/layout/NavBar'
 import Dashboard from './components/dashboard/Dashboard'
 import ProjectDetails from './components/project/ProjectDetails'
+import CreateProject from './components/project/CreateProject'
 import SignIn from './components/auth/SignIn'
 import SignUp from './components/auth/SignUp'
 
@@ -16,6 +17,7 @@ function App() {
           <Route path="/project/:id" component={ProjectDetails}/>
           <Route path="/signin" component={SignIn}/>
           <Route path="/signup" component={SignUp}/>
+          <Route path="/create" component={CreateProject}/>
         </Switch>
       </div>
     </Router>
---
+++ src/components/project/CreateProject.jsx
@@ -0,0 +1,45 @@
+import React,{useState} from 'react'
+
+const CreateProject=()=> {
+
+    let [userData,setUserData]=useState({
+        title:'',
+        content:''
+    })
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        console.log(userData);
+        
+    }
+    const handleChange=(e)=>{
+        setUserData((preValue)=>{
+            return {
+                ...preValue,
+                [e.target.id]:e.target.value
+            }
+        })
+    }
+    
+
+    return (
+        <div className="container">
+            <form className="white" onSubmit={handleSubmit}>
+            <h5 className="grey-text text-darken-3">Create New Project</h5>
+            <div className="input-field">
+                <label htmlFor="title">Title</label>
+                <input type="text" id='title' onChange={handleChange} />
+            </div>
+            <div className="input-field">
+                <label htmlFor="content">Project Content</label>
+                <textarea  id="content" className="materialize-textarea" onChange={handleChange}></textarea>
+            </div>
+            <div className="input-field">
+                <button className="btn pink lighten-1 z-depth-0">Create Project</button>
+            </div>
+            </form>
+        </div>
+    )
+}
+
+export default CreateProject
