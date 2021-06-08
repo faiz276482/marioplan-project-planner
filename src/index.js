32bfd56f84dd87b1c78ae56d0c895257969d3336
Faiz Alam <faiz276482@gmail.com>
2021-06-08 20:20:49 +0530

    Adding data to firestore

--- src/index.js
+++ src/index.js
@@ -3,18 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore,applyMiddleware,compose } from 'redux';
-import rootReducer from './store/reducers/rootReducer';
-import {Provider} from 'react-redux';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux'
+import rootReducer from './store/reducers/rootReducer'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 import { reduxFirestore, getFirestore } from 'redux-firestore';
 import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
-import fbConfig from './config/fbConfig';
+import fbConfig from './config/fbConfig'
 
-const store = createStore(rootReducer,compose(
-  applyMiddleware(thunk.withExtraArgument({ getFirestore,getFirebase })),
-  reduxFirestore(),
-  reactReduxFirebase()
+const store = createStore(rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+    reactReduxFirebase(fbConfig), // redux binding for Firebase
+    reduxFirestore(fbConfig) // redux bindings for firestore
   )
 );
 
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,20 @@
 export const createProject = (project) => {
     return (dispatch, getState,{ getFirestore,getFirebase }) => {
       // make async call to database
-      dispatch({ type: 'CREATE_PROJECT', project });
+      const firestore = getFirestore();
+      firestore.collection('projects').add({ 
+        ...project,
+        authorFirstName:'Faiz',
+        authorLastName: 'Alam',
+        authorId: 12345,
+        timestamp: new Date(),
+      })
+      .then(()=>
+          {
+            dispatch({ type: 'CREATE_PROJECT', project });
+          })
+      .catch(err =>{
+        dispatch({ type: 'CREATE_PROJECT_ERROR',err });
+      });
     }
 };
