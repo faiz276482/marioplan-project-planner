c49d044a6d71c625ae32b409a5c19e2083cf71bd
Faiz Alam <faiz276482@gmail.com>
2021-06-10 18:52:35 +0530

    Fetching individual project details from firestore using firestoreConnect

--- src/components/project/ProjectList.jsx
+++ src/components/project/ProjectList.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import ProjectSummary from './ProjectSummary';
+  
+import React from 'react'
+import ProjectSummary from './ProjectSummary'
+import { Link } from 'react-router-dom'
 
-function ProjectList({projects}) {
-    // console.log(project)
-    return (
-        <div className="project-list section">
-
-            {projects && projects.map(project =>{
-                return(
-                    <ProjectSummary project={project} key={project.id} />
-                )
-            })}
-
-        </div>
-    )
+const ProjectList = ({projects}) => {
+  return (
+    <div className="project-list section">
+      { projects && projects.map(project => {
+        return (
+          <Link to={'/project/' + project.id} key={project.id}>
+            <ProjectSummary project={project} />
+          </Link>
+        )
+      })}  
+    </div>
+  )
 }
 
 export default ProjectList
