974aa1a28ba05985cd467c2ea2ac62fdfc0aa0a0
Faiz Alam <faiz276482@gmail.com>
2021-06-11 14:15:30 +0530

    Logging out users implemented

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -13,8 +13,12 @@ const initState = {
       case 'LOGIN_SUCCESS':
         console.log('login success');
         return {
+          ...state,
           authError: null
         }
+      case 'SIGNOUT_SUCCESS':
+        console.log('signout success');
+        return state
       default:
         return state
     }
